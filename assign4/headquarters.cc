038dfa8ebe143a530ac50cc24d72fca4e58c7240
Michael Banks <banmic99@hotmail.com>
2013-01-26 15:53:12 -0700

    Update warehouse.cc and .h to have comments and etc.

--- assign4/headquarters.cc
+++ assign4/headquarters.cc
@@ -7,3 +7,73 @@
  *
  *
  */
+#include "warehouse.h"
+
+warehouse::warehouse(string name, set<food_item> &foodSet){
+  *foods = foodSet;
+
+}
+
+warehouse::~warehouse(){
+
+}
+
+/*
+ * Is sent items that have been received by the warehouse, 
+ * adds them to the data structure of transactions.
+ */
+void warehouse::receive(string theReceive){
+
+
+}
+
+/*
+ * Is sent items that have been requested by the warehouse, 
+ * adds them to the data structure of transactions.
+ */
+void warehouse::request(string theRequest){
+
+
+}
+
+/*
+ * Returns name, date and transaction quantity of busiest day.
+ *
+ * Goes through all transactions by date and determines which date
+ * had the most transactions. It returns the transaction quantity.
+ * If two or more days have the same amount of transactions it returns 
+ * the first one.
+ */
+string warehouse::reportBusiestDay(){
+
+}
+
+/*
+ * At the end of the reporting period this function receives 
+ * a list of all the foods and checks to see whether its own list
+ * has a deficit.  If a certain item is not in stock it will be added to
+ * the list that is being returned.
+ */
+string warehouse::reportFoodDeficit(){
+
+}
+
+/*
+ * At the end of the reporting period this function receives 
+ * a list of all the foods and checks to see whether its own list
+ * has a surplus.  If a certain item is in stock it will be added to
+ * the list that is being returned.
+ */
+list<food_item>  warehouse::reportFoodsInStock(const list<food_item> &foods){
+
+
+
+}
+
+
+/*
+ * This function is called when it is the next day.
+ */
+void warehouse::forwardDate(){
+
+}
--- assign4/warehouse.h
+++ assign4/warehouse.h
@@ -10,27 +10,31 @@
 #define WAREHOUSE_HEADER
 
 #include <string>
+#include <set>
+#include <list>
+#include "food_item.cc"
+#include "date.cc"
 
 using namespace std;
 
 class warehouse{
  public:
-  warehouse(string name);
+  warehouse(string name, set<food_item> &foodSet);
   ~warehouse();
-  /**/
-  void receive();
-  void request();
-  string reportBusiestDay();
-  string effectiveDate();
+
+  void receive(string theReceive);
+  void request(string theRequest);
+  string reportBusiestDay();        // Returns name, date and transaction quantity of busiest day.
   string reportFoodDeficit();
-  string reportFoodSurplus();
+  list<food_item> reportFoodsInStock();
   void forwardDate();
 
  private:
   string name;
-
-
+  date effectiveDate;
+  set<food_item> *foods;
   string convertInt_toString();
 
 
 };
+#endif
