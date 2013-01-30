0e599b754f54e04152518f9d9566748caa98baea
Michael Banks <banmic99@hotmail.com>
2013-01-30 15:55:47 -0700

    Added makefile and edited food_item.cc

--- assign4/food_item.cc
+++ assign4/food_item.cc
@@ -14,24 +14,33 @@ using namespace std;
 
 class food_item {
 public:
+  string getUPC();
+  string getName();
+ void decShelfLife();
+private:
   string upc_code;
-  string shelf_life;
+  int shelf_life;
   string name;
+ 
 
-  food_item(const string upc_code, const string shelf_life, const string name); // public constructor
+  food_item(const string upc_code,int shelf_life, const string name); // public constructor
 };
 
 
 
 // implementation
-food_item::food_item(const string upc_code, const string shelf_life, const string name) {
+food_item::food_item(const string upc_code,int  shelf_life, const string name) {
   this->upc_code = upc_code;
   this->shelf_life = shelf_life;
   this->name = name;
 
 }
 
-
+void food_item::decShelfLife()
+{
+  if(shelfLife > 0)
+    shelfLife--;
+}
 
 #endif
 
