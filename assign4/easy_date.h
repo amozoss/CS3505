ca45f5d4623ce494a562c0629b02bbac0264b634
Dan Willoughby <amozoss@gmail.com>
2013-01-30 15:40:41 -0700

    add tester.cc and easy_date.h

---
+++ assign4/easy_date.h
@@ -0,0 +1,34 @@
+/*
+ * Easy Date Class uses the boost library to keep track of dates. 
+ *
+ * The day can be incremented forward one day. 
+ *
+ *
+ * Authors: Dan Willoughby and Michael Banks
+ *
+ */
+#ifndef EASY_DATE_CLASS
+#define EASY_DATE_CLASS
+
+#include <string>
+#include <iostream>
+#include "boost/date_time/gregorian/gregorian.hpp" //include all types plus i/o
+#include "boost/date_time/gregorian/parsers.hpp"
+//#include <locale>
+//#include <boost/date_time.hpp>
+//using std::locale;
+
+//using namespace boost::local_time;
+using namespace boost::gregorian;
+
+//using namespace std;
+
+class easy_date {
+public:
+  easy_date(std::string &startDate); 
+  std::string nextDate();
+  std::string to_str();
+private:
+  date theDate;
+};
+#endif
