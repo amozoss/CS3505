abfb4c4da5f7775fc64f94b884d39bdb1fb7cb15
Michael Banks <banmic99@hotmail.com>
2013-01-30 17:30:51 -0700

    Worked on transaction.cc

--- assign4/transaction.cc
+++ assign4/transaction.cc
@@ -7,11 +7,53 @@
  */
 
 #include "transaction.h"
+#include <vector>
 
-transaction::transaction(string s){
+transaction::transaction(string s)
+{
+  if(s[2] == 'c')
+    type_of_transaction = receive;
+
+  vector<string> words = s.split(' ');
+
+  upc_code = words[2];
+
+  cout << "Transaction type " << type_of_transaction << endl;
+
+  cout << upc_code << endl;
+
+  shelf_life = atoi(words[3].c_str());
+
+  cout << shelf_life << " is the shelf life." << endl;
+
+
+
 
 }
 
-//transaction::~transaction(){
+transaction::~transaction()
+{
 
-//}
+}
+
+
+
+int transaction::get_type()
+{
+  return type_of_transaction;
+}
+
+int transaction::get_upc_code()
+{
+
+}
+
+string transaction::get_date()
+{
+
+}
+
+int transaction::get_quantity()
+{
+
+}
