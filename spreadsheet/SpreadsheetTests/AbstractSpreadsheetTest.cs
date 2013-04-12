9675658e3cd9cdf4c904dedc77658fbc79769efe
Dan Willoughby <amozoss@gmail.com>
2013-04-12 00:22:53 -0600

    readding projects

---
+++ spreadsheet/SpreadsheetTests/AbstractSpreadsheetTest.cs
@@ -0,0 +1,135 @@
+﻿using SS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using SpreadsheetUtilities;
+
+namespace SpreadsheetTests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for AbstractSpreadsheetTest and is intended
+    ///to contain all AbstractSpreadsheetTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class AbstractSpreadsheetTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        internal virtual AbstractSpreadsheet CreateAbstractSpreadsheet()
+        {
+            // TODO: Instantiate an appropriate concrete class.
+            AbstractSpreadsheet target = null;
+            return target;
+        }
+
+        internal virtual AbstractSpreadsheet_Accessor CreateAbstractSpreadsheet_Accessor()
+        {
+            // TODO: Instantiate an appropriate concrete class.
+            AbstractSpreadsheet_Accessor target = null;
+            return target;
+        }
+        /*
+        /// <summary>
+        ///A test for GetCellsToRecalculate
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("Spreadsheet.dll")]
+        public void GetCellsToRecalculateTest()
+        {
+            PrivateObject param0 = null; // TODO: Initialize to an appropriate value
+            AbstractSpreadsheet_Accessor target = new AbstractSpreadsheet_Accessor(param0); // TODO: Initialize to an appropriate value
+            ISet<string> names = null; // TODO: Initialize to an appropriate value
+            IEnumerable<string> expected = null; // TODO: Initialize to an appropriate value
+            IEnumerable<string> actual;
+            actual = target.GetCellsToRecalculate(names);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for GetCellsToRecalculate
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("Spreadsheet.dll")]
+        public void GetCellsToRecalculateTest1()
+        {
+            PrivateObject param0 = null; // TODO: Initialize to an appropriate value
+            AbstractSpreadsheet_Accessor target = new AbstractSpreadsheet_Accessor(param0); // TODO: Initialize to an appropriate value
+            string name = string.Empty; // TODO: Initialize to an appropriate value
+            IEnumerable<string> expected = null; // TODO: Initialize to an appropriate value
+            IEnumerable<string> actual;
+            actual = target.GetCellsToRecalculate(name);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+       
+        /// <summary>
+        ///A test for Visit
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("Spreadsheet.dll")]
+        public void VisitTest()
+        {
+            PrivateObject param0 = null; // TODO: Initialize to an appropriate value
+            AbstractSpreadsheet_Accessor target = new AbstractSpreadsheet_Accessor(param0); // TODO: Initialize to an appropriate value
+            string start = string.Empty; // TODO: Initialize to an appropriate value
+            string name = string.Empty; // TODO: Initialize to an appropriate value
+            ISet<string> visited = null; // TODO: Initialize to an appropriate value
+            LinkedList<string> changed = null; // TODO: Initialize to an appropriate value
+            target.Visit(start, name, visited, changed);
+            Assert.Inconclusive("A method that does not return a value cannot be verified.");
+        }
+       * */
+    }
+}
---
+++ spreadsheet/SpreadsheetTests/CodedUITest1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+
+
+namespace SpreadsheetTests
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class CodedUITest1
+    {
+        ApplicationUnderTest app;
+        public CodedUITest1()
+        {
+        }
+
+        [TestMethod]
+        public void CodedUITest_simpleAdd()
+        {
+
+            this.UIMap.adding_formula();
+
+           
+
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            // For more information on generated code, see http://go.microsoft.com/fwlink/?LinkId=179463
+        }
+
+        [TestMethod]
+        public void CodedUITest_saveAs()
+        {
+
+          
+            this.UIMap.save_test_1();
+
+
+
+            this.UIMap.Testing_saveas();
+            this.UIMap.save_existing();
+        }
+
+    
+
+        [TestMethod]
+        public void CodedUITest_saveAs_extensions()
+        {
+
+           // this.UIMap.save_with_different_ext();
+            this.UIMap.insure_ss_ext();
+
+           
+        }
+
+        [TestMethod]
+        public void CodedUITest_openAndSave()
+        {
+
+            this.UIMap.open_wrong_ext();
+
+            this.UIMap.save_open2();
+
+            //  this.UIMap.open_save1();
+
+
+
+        }
+
+        [TestMethod]
+        public void CodedUITest_invalidFormula()
+        {
+
+            this.UIMap.invalidFormula_syntax();
+
+            this.UIMap.circular_ex();
+
+            // this.UIMap.invalidSyntax_circularException();
+
+            this.UIMap.invalidFormula();
+            this.UIMap.Test_close_button1();
+        }
+
+        [TestMethod]
+        public void CodedUITest_menuTesting()
+        {
+
+           
+            this.UIMap.file_new();
+
+            this.UIMap.help_menu();
+
+
+            this.UIMap.close();
+         
+        }
+        [TestMethod]
+        public void CodedUITest_closeMenuTesting1()
+        {
+
+
+            
+            this.UIMap.closeButton_save();
+           
+        }
+        [TestMethod]
+        public void CodedUITest_closeMenuTesting2()
+        {
+
+
+
+            
+            this.UIMap.menuClose_save();
+        }
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:
+
+        //Use TestInitialize to run code before running each test 
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {        
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            // For more information on generated code, see http://go.microsoft.com/fwlink/?LinkId=179463
+            app = ApplicationUnderTest.Launch(@"..\..\..\SpreadsheetGUI\bin\Debug\SpreadsheetGUI.exe");
+
+        }
+
+        //Use TestCleanup to run code after each test has run
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {        
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            // For more information on generated code, see http://go.microsoft.com/fwlink/?LinkId=179463
+            app.Close();
+        }
+
+        #endregion
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
